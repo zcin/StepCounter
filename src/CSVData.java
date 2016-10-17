3488f9722bbfc60132c9df9eeccdb7f4696eada5
Cindy Zhang <cindyzyx9@gmail.com>
2016-10-16 19:00:53 -0700

    added method stubs and javadoc comments

--- src/CSVData.java
+++ src/CSVData.java
@@ -1,4 +1,196 @@
 
 public class CSVData {
+	private double[][] data;
+	private String[] columnNames;
 
+	public CSVData() {
+
+	}
+
+	/***
+	 * returns a new CSVData object for a file ignoring the lines at the top It
+	 * uses the first row as the column names. all other data is stored in 2D
+	 * array
+	 * 
+	 * @param filename
+	 *            name of the CSV file
+	 * @param numLinesIgnore
+	 *            the number of lines to ignore from the top of the file
+	 * @param columnHeaders
+	 *            the names/descriptions of each column of values
+	 * @return CSVData
+	 */
+	public static CSVData readCSVFile(String filename, int numLinesIgnore, String[] columnHeaders) {
+		return new CSVData();
+	}
+
+	/***
+	 * returns a new CSVData object for a file ignoring the lines at the top It
+	 * uses the first row as the column names. all other data is stored in 2D
+	 * array
+	 * 
+	 * @param filename
+	 *            name of the CSV file
+	 * @param numLinesIgnore
+	 *            the number of lines to ignore from the top of the file
+	 * @return CSVDATA
+	 */
+	public static CSVData readCSVFile(String filename, int numLinesIgnore) {
+		return new CSVData();
+	}
+
+	/***
+	 * returns the individual row out of the array
+	 * 
+	 * @param rowIndex
+	 *            index of the row
+	 * @return the array of the rows
+	 */
+
+	public double[] getRow(int rowIndex) {
+		return null;
+	}
+
+	/***
+	 * returns the individual col out of the array
+	 * 
+	 * @param columnIndex
+	 *            index of the col
+	 * @return returns the array of the columns
+	 */
+
+	public double[] getColumn(int columnIndex) {
+		return null;
+	}
+
+	/***
+	 * returns the individual col out of the array
+	 * 
+	 * @param name
+	 *            the name of the col
+	 * @return returns the name of the columns
+	 */
+
+	public double[] getColumn(String name) {
+		return null;
+	}
+
+	/***
+	 * returns multiple rows out of the data array
+	 * 
+	 * @param startIndex
+	 *            the index you are starting at
+	 * @param endIndex
+	 *            the index you are ending at
+	 * @return returns the array of the rows
+	 */
+
+	public double[][] getRows(int startIndex, int endIndex) {
+		return null;
+	}
+
+	/***
+	 * returns multiple rows out of the data array
+	 * 
+	 * @param rowIndexes
+	 *            the array of the indexes of the row
+	 * @return returns the array of the rows
+	 */
+
+	public double[][] getRows(int[] rowIndexes) {
+		return null;
+	}
+
+	/***
+	 * returns the multiple columns out of the data array
+	 * 
+	 * @param columnIndexes
+	 *            the array of the indexes of the columns
+	 * @return returns the array of the columns
+	 */
+
+	public double[][] getColumns(int[] columnIndexes) {
+		return null;
+	}
+
+	/***
+	 * returns the multiple rows out of the data array
+	 * 
+	 * @param startIndex
+	 *            the index you are starting at
+	 * @param endIndex
+	 *            the index you are ending at
+	 * @return returns the array of the columns
+	 */
+	public double[][] getColumns(int startIndex, int endIndex) {
+		return null;
+	}
+
+	/***
+	 * returns the multiple columns out of the data array
+	 *
+	 * @param colNames
+	 *            the name of the column
+	 * @return returns the array of the columns
+	 */
+
+	public double[][] getColumns(String colNames) {
+		return null;
+	}
+
+	/***
+	 * sets the value of the the element in the array
+	 * 
+	 * @param rowIndex
+	 *            the index of the row
+	 * @param colIndex
+	 *            the index of the column
+	 * @param newValue
+	 *            the value you want to set the element to
+	 */
+	public void setValue(int rowIndex, int colIndex, double newValue) {
+
+	}
+
+	/***
+	 * sets the values of the rows
+	 * 
+	 * @param values
+	 *            the array of the row values
+	 */
+
+	public void setRow(double[] values) {
+
+	}
+
+	/***
+	 * sets the values of the columns
+	 * 
+	 * @param values
+	 *            the array of the col values
+	 */
+
+	public void setColumn(double[] values) {
+
+	}
+
+	/***
+	 * gets the names of the columns
+	 * 
+	 * @return returns the array of the column names
+	 */
+
+	public String[] getColumnTitles() {
+		return null;
+	}
+
+	/***
+	 * saves the current state of the object back into a CSV
+	 * 
+	 * @param filename
+	 *            the name of the file
+	 */
+	public void saveToFile(String filename) {
+
+	}
 }
