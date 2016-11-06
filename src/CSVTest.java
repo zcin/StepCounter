5bbc48023bf1eb91b47805534b6b5a56395a03ae
Rohan Rodrigues <rohanrodrigues19@gmail.com>
2016-11-05 18:12:30 -0700

    Added separate algorithm that takes in CSV data files from the iPhone app Sensor Data, and converts it into the same format as the HyperIMU CSV data file

---
+++ src/CSVTest.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class CSVTest {
+
+	public static void main(String[] args) throws IOException {
+		CSVIphoneToAndroid.iphoneToAndroidFormat("/Users/rohanrodrigues/Documents/workspace_mars/StepCounter/data/jog1.txt");
+	}
+
+}
