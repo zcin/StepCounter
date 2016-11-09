3479c4990cfa87f2df1baef8a9bad12ecdf40ab1
Cindy Zhang <cindyzyx9@gmail.com>
2016-11-08 16:57:11 -0800

    delete unused classes

--- src/CSVTest.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class CSVTest {
-
-	public static void main(String[] args) throws IOException {
-		CSVIphoneToAndroid.iphoneToAndroidFormat("/Users/rohanrodrigues/Documents/workspace_mars/StepCounter/data/jog1.txt");
-	}
-
-}
